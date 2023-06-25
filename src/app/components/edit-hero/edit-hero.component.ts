b95934b6520024d2318786472ad1f8c27c215e86
maicol hernandez <maykolandres4@gmail.com>
2023-06-25 07:00:35 -0500

    finsih

--- src/app/components/edit-hero/edit-hero.component.ts
+++ src/app/components/edit-hero/edit-hero.component.ts
@@ -1,22 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SuperHero } from 'src/app/models/superhero';
+import { SuperheroService } from 'src/app/services/superhero.service';
 
 @Component({
   selector: 'app-edit-hero',
   templateUrl: './edit-hero.component.html',
   styleUrls: ['./edit-hero.component.css']
 })
-export class EditHeroComponent {
+export class EditHeroComponent implements OnInit {
   @Input() hero?: SuperHero;
+  @Output() heroesUpdated = new  EventEmitter<SuperHero[]>();
+
+  constructor(private superheroService: SuperheroService){}
 
   ngOnInit () : void {
 
   }
 
-  updateHero (hero: SuperHero) {}
+  updateHero (hero: SuperHero) {
+    this.superheroService
+    .updateHero(hero)
+    .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes)); 
+  }
 
-  deleteHero (hero: SuperHero) {}
+  deleteHero (hero: SuperHero) {
+    this.superheroService
+    .deleteHero(hero)
+    .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes)); 
+  }
 
-  createHero (hero: SuperHero) {}
+  createHero (hero: SuperHero) {
+    this.superheroService
+    .createHero(hero)
+    .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes)); 
+  }
 
 }
